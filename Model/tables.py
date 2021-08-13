3da0d7ac1a9a7e8e6ec32b7186587829234e7120
turamant <tur1amant@gmail.com>
2021-08-14 01:09:20 +0300

    финиш

---
+++ Model/tables.py
@@ -0,0 +1,26 @@
+from sqlalchemy import MetaData, Table, Column, Integer, String, \
+    DateTime, Boolean, ForeignKey
+
+metadata = MetaData()
+users = Table('users', metadata,
+              Column('id', Integer(), primary_key=True, nullable=False),
+              Column('login', String(100), unique=True, nullable=False),
+              Column('password', String(100), nullable=False),
+              Column('data_create_user', DateTime()),
+              )
+tasks = Table('tasks', metadata,
+              Column('id', Integer(), primary_key=True, nullable=False),
+              Column('title', String(100), unique=True),
+              Column('note', String(100), nullable=False),
+              Column('status', Boolean(), default=0),
+              Column('data_create_task', DateTime()),
+
+              Column('data_take_work', DateTime()),
+              Column('take_work_status', Boolean(), default=0),
+              Column('data_get_done', DateTime()),
+              Column('get_done_status', Boolean(), default=0),
+              Column('delete_status', Boolean(), default=0),
+              Column('cancel_status', Boolean(), default=0),
+              Column('data_cancel', DateTime()),
+              Column('user_id', ForeignKey("users.id")),
+              )
--- app.py
+++ app.py
@@ -1,48 +1,31 @@
 ##################################################################
 #
-#  Трекер задач
-#       (12.08.)
-#################################################################
+#          Трекер задач
+#       (12.08.) - (13.08)
+#         ....askvart.....
+##################################################################
 
+import os
 import datetime
 from flask import Flask, g, request, render_template, flash, url_for, session
-from sqlalchemy import MetaData, Table, Column, Integer, String, DateTime, Boolean, ForeignKey, create_engine, select
+from sqlalchemy import create_engine, select, or_
+from sqlalchemy.sql import func
 from werkzeug.utils import redirect
-import os
+
+from Model.tables import users, tasks, metadata
 
 DATABASE = '/tmp/todo_002.db'
 DEBUG = True
 SECRET_KEY = 'fdgfh78@#5?>gfhf89dx,v06k'
 USERNAME = 'admin'
 PASSWORD = '123'
+STATIC_FOLDER = 'static'
+
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.update(dict(DATABASE=os.path.join(app.root_path, 'todo_002.db')))
 
-metadata = MetaData()
-users = Table('users', metadata,
-              Column('id', Integer(), primary_key=True, nullable=False),
-              Column('login', String(100), unique=True, nullable=False),
-              Column('password', String(100), nullable=False),
-              Column('data_create_user', DateTime()),
-              )
-tasks = Table('tasks', metadata,
-              Column('id', Integer(), primary_key=True, nullable=False),
-              Column('title', String(100), unique=True),
-              Column('note', String(100), nullable=False),
-              Column('user_id', ForeignKey("users.id")),
-              )
-
-task_status = Table('task_status', metadata,
-                    Column('task_id', ForeignKey("tasks.id"), primary_key=True),
-                    Column('status', Boolean(), default=0),
-                    Column('data_create_task', DateTime()),
-                    Column('data_take_work', DateTime()),
-                    Column('take_work_status', Boolean(), default=0),
-                    Column('data_get_done', DateTime()),
-                    Column('get_done_status', Boolean(), default=0),
-                    )
 
 
 def connect_db():
@@ -63,78 +46,166 @@ def close_db(error):
     if hasattr(g, 'link_db'):
         g.link_db.close()
 
+
 def get_users():
+    '''Список пользователей'''
     db = get_db()
     query = select([users])
     r = db.execute(query)
     rows = r.fetchall()
     return rows
 
+def get_users_one():
+    '''заглушка'''
+    db = get_db()
+    query = select([users])
+    r = db.execute(query)
+    rows = r.fetchone()
+    return rows[1]
+
+
+
 def get_tasks_open():
+    '''Список открытых задач'''
     db = get_db()
     query = select([tasks]).where(
-        tasks.c.status == 0
+        tasks.c.cancel_status == False,
+        tasks.c.status == False,
     )
     r = db.execute(query)
     rows = r.fetchall()
     return rows
 
+def get_tasks_take_work():
+    '''Список взятых в работу задач'''
+    db = get_db()
+    query = select([tasks]).where(
+        tasks.c.take_work_status == True,
+        tasks.c.get_done_status == False,
+    )
+    r = db.execute(query)
+    rows = r.fetchall()
+    return rows
+
+def get_tasks_archive():
+    '''Список задач отправленных в архив(выполненные и отмененные)'''
+    db = get_db()
+    query = select([tasks]).where(
+        or_(
+            tasks.c.delete_status == True,
+            tasks.c.cancel_status == True,
+        )
+    ).order_by(tasks.c.cancel_status)
+    r = db.execute(query)
+    rows = r.fetchall()
+    return rows
+
+def statistic_all_tasks():
+    ''' Статистика всех заданий (на текущее время) '''
+    db = get_db()
+    cdb = [
+        func.count(tasks.c.id)
+    ]
+    query = select(cdb)
+    r = db.execute(query)
+    rows = r.fetchone()
+    return rows[0]
+
+def statistic_cancel_tasks():
+    ''' Статистика всех отмененных заданий (на текущее время)'''
+    db = get_db()
+    cdb = [
+        func.count(tasks.c.id)
+    ]
+    query = select(cdb).where(
+        tasks.c.cancel_status == True
+    )
+    r = db.execute(query)
+    rows = r.fetchone()
+    return rows[0]
+
+
+def statistic_open_tasks():
+    ''' Статистика всех открытых заданий (на текущее время)'''
+    db = get_db()
+    cdb = [
+        func.count(tasks.c.id)
+    ]
+    query = select(cdb).where(
+        tasks.c.cancel_status == False,
+        tasks.c.status == False,
+    )
+    r = db.execute(query)
+    rows = r.fetchone()
+    return rows[0]
+
+def statistic_take_work_tasks():
+    ''' Статистика всех заданий находящихся в работе (на текущее время)'''
+    db = get_db()
+    cdb = [
+        func.count(tasks.c.id)
+    ]
+    query = select(cdb).where(
+        tasks.c.take_work_status == True,
+        tasks.c.get_done_status == False,
+    )
+    r = db.execute(query)
+    rows = r.fetchone()
+    return rows[0]
+
+
+def statistic_get_done_tasks():
+    ''' Статистика всех выполненных заадний (на текущее время)'''
+    db = get_db()
+    cdb = [
+        func.count(tasks.c.id)
+    ]
+    query = select(cdb).where(
+        tasks.c.get_done_status == True
+    )
+    r = db.execute(query)
+    rows = r.fetchone()
+    return rows[0]
+
+
+
+# Страничка статистики
+@app.route('/statistics')
+def statistics():
+    '''стастистика '''
+    return render_template("statistics.html",
+                           count_all_tasks=statistic_all_tasks(),
+                           count_done_tasks=statistic_get_done_tasks(),
+                           count_all_cancel_tasks=statistic_cancel_tasks(),
+                           count_all_open_tasks=statistic_open_tasks(),
+                           count_take_work_tasks=statistic_take_work_tasks(),
+                           )
+
+
+
+##### Кнопка взять задание в работу
 @app.route("/selecting/<int:task_id>")
 def selecting(task_id):
-    '''Обновить статус на выполнено'''
+    '''Кнопка взять в работу'''
     db = get_db()
     query = select([tasks]).where(
         tasks.c.id == task_id
     )
     r = db.execute(query)
     rows = r.fetchone()
-
     if rows != None:
         query2 = tasks.update().where(
         tasks.c.id == task_id
         ).values(
             data_take_work=datetime.datetime.now(),
             take_work_status=True,
-            get_done_status=False,
         )
         db.execute(query2)
     return redirect(url_for("home"))
 
-
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    '''Главная страница
-    Управление сессией'''
-    if not session.get('logged_in'):
-        return render_template('index.html')
-    else:
-        if request.method == 'GET':
-            db = get_db()
-            query = select([tasks])
-            r = db.execute(query)
-            rows = r.fetchall()
-            return render_template("index.html", task_list=rows, data=get_users(), user_list=get_users())
-
-@app.route("/add", methods=["POST"])
-def add():
-    '''Добавить задания'''
-    db = get_db()
-    if request.method == "POST":
-        if len(request.form['title']) > 5 and len(request.form['note']) > 10:
-            query = tasks.insert().values(
-                title=request.form.get("title"),
-                note=request.form.get("note"),
-                status=False,
-                data_create_task=datetime.datetime.now(),
-                data_take_work=datetime.datetime.now(),
-                data_get_done=datetime.datetime.now(),
-                user_id=request.form.get("user_id"),
-            )
-            db.execute(query)
-    return redirect(url_for("home"))
-
-@app.route("/update/<int:task_id>")
-def update(task_id):
+##### Кнопка задание выполнено
+@app.route("/workdone/<int:task_id>")
+def workdone(task_id):
     '''Обновить статус на выполнено'''
     db = get_db()
     query = select([tasks]).where(
@@ -146,13 +217,120 @@ def update(task_id):
         query2 = tasks.update().where(
         tasks.c.id == task_id
         ).values(
-            status=True
+            data_get_done=datetime.datetime.now(),
+            get_done_status=True,
         )
         db.execute(query2)
     return redirect(url_for("home"))
 
+
+#Страничка архива списка задач
+@app.route('/tasksarchive')
+def tasksarchive():
+    '''открытые задания '''
+    return render_template("tasks_archive.html", task_list=get_tasks_archive())
+
+#Страничка списка открытых задач
+@app.route('/tasksopen')
+def tasksopen():
+    '''открытые задания '''
+    return render_template("tasks_open.html", task_list=get_tasks_open())
+
+# Страничка списка взятых в работу
+@app.route('/taskstakework')
+def taskstakework():
+    '''взятые в работу '''
+    return render_template("tasks_take_work.html", task_list=get_tasks_take_work())
+
+
+def get_task_list():
+    '''Показать все доступные задачи'''
+    db = get_db()
+    query = select([tasks]).where(
+        tasks.c.delete_status == False,
+        tasks.c.cancel_status == False,
+    )
+    r = db.execute(query)
+    rows = r.fetchall()
+    return rows
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    '''Главная страница управление сессией'''
+    if not session.get('logged_in'):
+        return render_template('index.html')
+    else:
+        if request.method == 'GET':
+            return render_template("index.html", task_list=get_task_list(), data=get_users_one(), user_list=get_users())
+
+def add_task():
+    '''Процедура  для функции add()'''
+    db = get_db()
+    query = tasks.insert().values(
+        title=request.form.get("title"),
+        note=request.form.get("note"),
+        status=False,
+        data_create_task=datetime.datetime.now(),
+        user_id=request.form.get("user_id"),
+    )
+    db.execute(query)
+
+
+@app.route("/add", methods=["POST"])
+def add():
+    '''Добавить новое задание'''
+    if request.method == "POST":
+        if len(request.form['title']) > 5 and len(request.form['note']) > 10:
+            add_task()
+    return redirect(url_for("home"))
+
+
+def select_task(task_id):
+    db = get_db()
+    query = select([tasks]).where(
+        tasks.c.id == task_id
+    )
+    r = db.execute(query)
+    rows = r.fetchone()
+    return rows
+
+
+def update_task(task_id):
+    '''Процедура для функции update()'''
+    db = get_db()
+    rows = select_task(task_id)
+    if rows != None:
+        query2 = tasks.update().where(
+            tasks.c.id == task_id
+        ).values(
+            status=True
+        )
+        db.execute(query2)
+
+
+@app.route("/update/<int:task_id>")
+def update(task_id):
+    '''Обновить статус задания на выполнено'''
+    update_task(task_id)
+    return redirect(url_for("home"))
+
 @app.route("/delete/<int:task_id>", methods=['GET', 'POST'])
 def delete(task_id):
+    '''Удалить задание из списка активных (переместить в архив)'''
+    db = get_db()
+    rows = select_task(task_id)
+    if rows != None:
+        query2 = tasks.update().where(
+            tasks.c.id == task_id
+        ).values(
+            delete_status=True
+        )
+        db.execute(query2)
+
+    return redirect(url_for("home"))
+
+@app.route("/cancel/<int:task_id>", methods=['GET', 'POST'])
+def cancel(task_id):
     '''Удалить задание'''
     db = get_db()
     query = select([tasks]).where(
@@ -161,22 +339,25 @@ def delete(task_id):
     r = db.execute(query)
     rows = r.fetchone()
     if rows != None:
-        query2 = tasks.delete().where(
+        query2 = tasks.update().where(
             tasks.c.id == task_id
+        ).values(
+            cancel_status=True,
+            data_cancel=datetime.datetime.now()
         )
         db.execute(query2)
 
     return redirect(url_for("home"))
 
+
 def create_tables():
+    '''запускается один раз, в случае обновления схемы таблиц,
+     или создания новой БД. Запустить можно с консоли '''
     metadata.create_all(connect_db().engine)
 
-@app.route('/tasksopen')
-def tasksopen():
-    '''открытые задания '''
-    return render_template("tasks_open.html", task_list=get_tasks_open())
 
 def select_login_user(log, passw):
+    '''Вспомогательная функция для функции login()'''
     db = get_db()
     query = select([users]).where(
         users.c.login == log).where(
@@ -221,8 +402,10 @@ def logout():
     session['logged_in'] = False
     return redirect(url_for('home'))
 
-
+# для создания таблиц при инициализации БД
 #metadata.create_all(connect_db().engine)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
     app.run(host='127.0.0.1')
