d245f4f9012bb902688f64d51b1d000b34974c06
Victor Turamant <60545309+turamant@users.noreply.github.com>
2021-10-26 14:26:26 +0300

    Update app.py
    app

--- app.py
+++ app.py
@@ -381,5 +381,5 @@ def logout():
     return redirect(url_for('home'))
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    app.run(host='127.0.0.1')
+    app.run(debug=True, port=8000, host='127.0.0.1')
+    
